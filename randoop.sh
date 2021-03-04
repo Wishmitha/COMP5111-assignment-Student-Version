69be1d3ccd7c6fc479d73e22af4dfbc2d7ed3387
wolfpack <wishmitha.14@cse.mrt.ac.lk>
2021-03-04 21:06:18 +0800

    add script to generate multiple random test suites

--- randoop.sh
+++ randoop.sh
@@ -2,9 +2,13 @@ RANDOOP_JAR="lib/randoop-all-4.2.5.jar"
 ROOT_DIR=$PWD
 TARGET=$ROOT_DIR"/target/classes"
 
-java -ea -classpath ${RANDOOP_JAR}:${TARGET}/ \
-   randoop.main.Main gentests \
-   --testclass=comp5111.assignment.cut.ToolBox \
-   --time-limit=60 \
-   --junit-package-name=comp5111.assignment.cut \
-   --junit-output-dir=${ROOT_DIR}/src/test/randoop0
+for i in 0 1 2 3 4
+do
+  java -ea -classpath ${RANDOOP_JAR}:${TARGET}/ \
+     randoop.main.Main gentests \
+     --testclass=comp5111.assignment.cut.ToolBox \
+     --randomseed=$i \
+     --time-limit=60 \
+     --junit-package-name=comp5111.assignment.cut \
+     --junit-output-dir=${ROOT_DIR}/src/test/randoop$i
+done
